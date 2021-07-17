6c89a1b50c791a2995aa13e6f5ab7894f2e50c2a
Viruales <petricevic.j@gmx.net>
2021-07-17 21:43:55 +0200

    Adds first Main.java.

---
+++ src/com/viruales/myfirstapp/Main.java
@@ -0,0 +1,8 @@
+package com.viruales.myfirstapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo das ist der erste versuch eines outputs");
+    }
+}
