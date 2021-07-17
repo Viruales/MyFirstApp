0f630fc5a838e8f0b24a9e542c7a1962b31cbe35
Viruales <petricevic.j@gmx.net>
2021-07-17 21:47:25 +0200

    adds new line

--- src/com/viruales/myfirstapp/Main.java
+++ src/com/viruales/myfirstapp/Main.java
@@ -3,6 +3,8 @@ package com.viruales.myfirstapp;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hallo das ist der erste versuch eines outputs");
+        System.out.println("Hier noch eine neue zeile");
     }
 }
